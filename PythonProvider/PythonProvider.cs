3363e6525a39f60cbf3f09a10f3ace23489a33f8
Vincent Povirk <madewokherd@gmail.com>
2014-11-15 15:46:00 -0600

    Initial commit.

---
+++ PythonProvider/PythonProvider.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics;
+using OneGet.ProviderSDK;
+using IRequestObject = System.Object;
+
+namespace PythonProvider
+{
+    public class PythonProvider
+    {
+        const string ProviderName = "Python";
+
+        public void InitializeProvider(IRequestObject requestImpl)
+        {
+            try
+            {
+                using (var request = requestImpl.As<Request>())
+                {
+                    request.Debug("Calling '{0}::InitializeProvider'", ProviderName);
+                }
+            }
+            catch (Exception e)
+            {
+                Debug.WriteLine(string.Format("Unexpected Exception thrown in '{0}::InitializeProvider' -- {1}\\{2}\r\n{3}"), ProviderName, e.GetType().Name, e.Message, e.StackTrace);
+            }
+        }
+
+        public string GetPackageProviderName()
+        {
+            return ProviderName;
+        }    }
+}
