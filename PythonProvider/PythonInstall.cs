c5febd6e0f3a9930700279a9650ba81cd3c21f13
Vincent Povirk <madewokherd@gmail.com>
2014-11-16 13:06:06 -0600

    Add class for managing python installs.

---
+++ PythonProvider/PythonInstall.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Diagnostics;
+using System.Collections.Generic;
+using System.IO;
+using OneGet.ProviderSDK;
+
+namespace PythonProvider
+{
+    class PythonInstall
+    {
+        public string install_path;
+        public string exe_path;
+        public string python_version;
+
+        private PythonInstall()
+        {
+        }
+
+        private string QueryPython(string arguments)
+        {
+            ProcessStartInfo startinfo = new ProcessStartInfo();
+            startinfo.FileName = exe_path;
+            startinfo.Arguments = arguments;
+            startinfo.RedirectStandardOutput = true;
+            startinfo.UseShellExecute = false;
+            Process proc = Process.Start(startinfo);
+            return proc.StandardOutput.ReadToEnd();
+        }
+
+        private string GetPythonVersion()
+        {
+            return QueryPython("-c \"import sys;sys.stdout.write(str(sys.version_info[0])+'.'+str(sys.version_info[1])+'.'+str(sys.version_info[2]))\"");
+        }
+
+        public static PythonInstall FromPath(string installpath, Request request)
+        {
+            try
+            {
+                PythonInstall result = new PythonInstall();
+                result.install_path = installpath;
+                result.exe_path = Path.Combine(installpath, "python.exe");
+                if (File.Exists(result.exe_path))
+                {
+                    result.python_version = result.GetPythonVersion();
+                    if (!string.IsNullOrEmpty(result.python_version))
+                        return result;
+                }
+            }
+            catch
+            {
+            }
+            return null;
+        }
+
+        private static void FindEnvironments(List<PythonInstall> result, bool win64, bool user, Request request)
+        {
+            using (RegistryKey basekey = RegistryKey.OpenBaseKey(
+                user ? RegistryHive.CurrentUser : RegistryHive.LocalMachine,
+                win64 ? RegistryView.Registry64 : RegistryView.Registry32))
+            {
+                RegistryKey pythoncore = basekey.OpenSubKey(@"Software\Python\PythonCore");
+                if (pythoncore != null)
+                {
+                    foreach (string version in pythoncore.GetSubKeyNames())
+                    {
+                        RegistryKey installpathkey = pythoncore.OpenSubKey(string.Format(@"{0}\InstallPath", version));
+                        if (installpathkey != null)
+                        {
+                            PythonInstall install = FromPath(installpathkey.GetValue(null).ToString(), request);
+                            if (install != null)
+                            {
+                                result.Add(install);
+                                request.Debug("Python::FindInstalledEnvironments found {0} in {1}", install.python_version, install.install_path);
+                            }
+                            installpathkey.Dispose();
+                        }
+                    }
+                    pythoncore.Dispose();
+                }
+            }
+        }
+
+        public static List<PythonInstall> FindEnvironments(Request request)
+        {
+            List<PythonInstall> result = new List<PythonInstall>();
+            if (Environment.Is64BitOperatingSystem)
+            {
+                FindEnvironments(result, true, false, request);
+                FindEnvironments(result, true, true, request);
+            }
+            FindEnvironments(result, false, false, request);
+            FindEnvironments(result, false, true, request);
+            return result;
+        }
+    }
+}
--- PythonProvider/PythonProvider.cs
+++ PythonProvider/PythonProvider.cs
@@ -31,67 +31,6 @@ namespace PythonProvider
             return ProviderName;
         }
 
-        private string GetPythonVersion(string exepath)
-        {
-            ProcessStartInfo startinfo = new ProcessStartInfo();
-            startinfo.FileName = exepath;
-            startinfo.Arguments = "-c \"import sys;sys.stdout.write(str(sys.version_info[0])+'.'+str(sys.version_info[1]))\"";
-            startinfo.RedirectStandardOutput = true;
-            startinfo.UseShellExecute = false;
-            Process proc = Process.Start(startinfo);
-            return proc.StandardOutput.ReadToEnd();
-        }
-
-        private void FindInstalledEnvironments(List<Tuple<string,string>> result, bool win64, bool user, Request request)
-        {
-            using (RegistryKey basekey = RegistryKey.OpenBaseKey(
-                user ? RegistryHive.CurrentUser : RegistryHive.LocalMachine,
-                win64 ? RegistryView.Registry64 : RegistryView.Registry32))
-            {
-                RegistryKey pythoncore = basekey.OpenSubKey(@"Software\Python\PythonCore");
-                if (pythoncore != null)
-                {
-                    foreach (string version in pythoncore.GetSubKeyNames())
-                    {
-                        RegistryKey installpathkey = pythoncore.OpenSubKey(string.Format(@"{0}\InstallPath", version));
-                        if (installpathkey != null)
-                        {
-                            try
-                            {
-                                string installpath = installpathkey.GetValue(null).ToString();
-                                string exepath = Path.Combine(installpath, "python.exe");
-                                if (File.Exists(exepath))
-                                {
-                                    string pythonversion = GetPythonVersion(exepath);
-                                    result.Add(new Tuple<string, string>(pythonversion, installpath));
-                                    request.Debug("Python::FindInstalledEnvironments found {0} in {1}", pythonversion, installpath);
-                                }
-                            }
-                            catch (Exception e)
-                            {
-                                request.Debug(string.Format("Unexpected Exception thrown in '{0}::FindInstalledEnvironments' -- {1}\\{2}\r\n{3}"), ProviderName, e.GetType().Name, e.Message, e.StackTrace);
-                            }
-                            installpathkey.Dispose();
-                        }
-                    }
-                    pythoncore.Dispose();
-                }
-            }
-        }
-
-        private List<Tuple<string, string>> FindInstalledEnvironments(Request request)
-        {
-            List<Tuple<string, string>> result = new List<Tuple<string, string>>();
-            if (Environment.Is64BitOperatingSystem)
-            {
-                FindInstalledEnvironments(result, true, false, request);
-                FindInstalledEnvironments(result, true, true, request);
-            }
-            FindInstalledEnvironments(result, false, false, request);
-            FindInstalledEnvironments(result, false, true, request);
-            return result;
-        }
-
         public void GetInstalledPackages(string name, object requestObject)
         {
             try
@@ -99,7 +38,7 @@ namespace PythonProvider
                 using (var request = requestObject.As<Request>())
                 {
                     request.Debug("Calling '{0}::GetInstalledPackages'", ProviderName);
-                    FindInstalledEnvironments(request); // Not really using this yet, just putting in a call for testing
+                    PythonInstall.FindEnvironments(request); // Not really using this yet, just putting in a call for testing
                 }
             }
             catch (Exception e)
