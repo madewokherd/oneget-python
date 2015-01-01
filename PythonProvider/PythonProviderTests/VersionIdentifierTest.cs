63d11f70f14ec8a6a06ff553a713bcd821a55990
Vincent Povirk <madewokherd@gmail.com>
2015-01-01 16:34:48 -0600

    Beginnings of PEP 440 support.

---
+++ PythonProvider/PythonProviderTests/VersionIdentifierTest.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using PythonProvider;
+
+namespace PythonProviderTests
+{
+    [TestClass]
+    public class VersionIdentifierTest
+    {
+        static readonly string[] sorted_version_strings = {
+                                                              "0",
+                                                              "0.9",
+                                                              "1.0",
+                                                              "1.1",
+                                                              "1.1.1",
+                                                              "1.2",
+                                                              "2"
+                                                          };
+
+        static readonly string[] string_normalizations = { "050.0.06", "50.0.6" };
+
+        [TestMethod]
+        public void TestNormalization()
+        {
+            foreach (var version_string in sorted_version_strings)
+            {
+                VersionIdentifier ver = new VersionIdentifier(version_string);
+                Assert.IsNull(ver.invalid_string, "version string {0} rejected by parser", version_string);
+                Assert.AreEqual(ver.ToString(), version_string, "version string did not normalize to itself");
+            }
+
+            int i;
+            for (i = 0; i < string_normalizations.Length; i+=2)
+            {
+                VersionIdentifier ver = new VersionIdentifier(string_normalizations[i]);
+                Assert.IsNull(ver.invalid_string, "version string {0} rejected by parser", string_normalizations[0]);
+                Assert.AreEqual(ver.ToString(), string_normalizations[i + 1], "expected {0} to normalize to {1}", string_normalizations[i], string_normalizations[i + 1]);
+            }
+        }
+    }
+}
---
+++ PythonProvider/VersionIdentifier.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PythonProvider
+{
+    public class VersionIdentifier
+    {
+        // see: https://www.python.org/dev/peps/pep-0440/
+
+        public int[] release;
+
+        public string invalid_string;
+
+        public VersionIdentifier(string version_string)
+        {
+            if (!ParseVersion(version_string))
+            {
+                invalid_string = version_string;
+            }
+        }
+
+        private bool ParseVersion(string version_string)
+        {
+            int pos = 0;
+
+            // release part
+            LinkedList<int> release_components = new LinkedList<int>();
+
+            // there must be at least one release component
+            if (pos >= version_string.Length || !char.IsDigit(version_string[pos]))
+            {
+                return false;
+            }
+
+            int release_component = 0;
+            do
+            {
+                release_component = release_component * 10 + version_string[pos] - '0';
+                pos++;
+            } while (pos < version_string.Length && char.IsDigit(version_string[pos]));
+
+            release_components.AddLast(release_component);
+
+            // any number of additional release components
+            while (pos < version_string.Length - 1 && version_string[pos] == '.' &&
+                char.IsDigit(version_string[pos+1]))
+            {
+                pos++;
+                release_component = 0;
+                do
+                {
+                    release_component = release_component * 10 + version_string[pos] - '0';
+                    pos++;
+                } while (pos < version_string.Length && char.IsDigit(version_string[pos]));
+                release_components.AddLast(release_component);
+            }
+
+            release = release_components.ToArray();
+
+            return true;
+        }
+
+        public override string ToString()
+        {
+            if (invalid_string != null)
+                return invalid_string;
+
+            StringBuilder result = new StringBuilder();
+
+            result.Append(release[0]);
+
+            for (int i = 1; i < release.Length; i++)
+            {
+                result.Append(".");
+                result.Append(release[i]);
+            }
+
+            return result.ToString();
+        }
+    }
+}
